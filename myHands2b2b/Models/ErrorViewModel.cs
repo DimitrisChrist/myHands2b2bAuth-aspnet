192daac73120332a2c4488fbe8b45e48c6169edb
Δημήτριος Χριστόπουλος 1 <d.christopoulos1@online.aade.gr>
2023-07-26 08:26:26 +0300

    Add project files.

---
+++ myHands2b2b/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace myHands2b2b.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
